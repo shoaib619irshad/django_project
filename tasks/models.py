11d8350895348503d303a8986c7a74be00d849c0
shoaib619irshad <alqqure@gmail.com>
2023-12-26 12:56:50 +0530

    Tasks Model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    TASK_STATUS = (
+        ('un', 'unassigned'),
+        ('r', 'running'),
+        ('p', 'pending'),
+        ('c', 'completed'),
+    ) 
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=2, choices=TASK_STATUS, default='un')
